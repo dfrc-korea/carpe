171a6f1cf25f851cc2bc4cb41ecd4d62b24d6e75
JJun1207 <jjun1207@korea.ac.kr>
2019-03-27 19:53:38 +0900

    update manager

--- manager.py
+++ manager.py
@@ -3,24 +3,49 @@ import json
 
 import pymysql
 import plaso
-from utility import carpe_db
+from utility import mariadb
 from image_analyzer import split_disk
 
 import pdb
 
 class CARPE_AM:
+	src_path = None
+	dst_path = None
+
 	def __init__(self):
-		print('Class init')
+		self.src_path = None
+		self.dst_path = None
 
-	def Image_Analysis(self, case_id, evd_id, user_id):
-		pdb.set_trace()
-		source = '/data/share/carpe/ntfs_par.001' 
-		output_dir = os.getcwd()
+	def Preprocess(self, case_id, evd_id, user_id):
+		# Connect MariaDB
+		db = mariadb.Mariadb()
+		conn = db.open()
 
+		# Get Source Path
+		query = 'SELECT file_path FROM tn_evidence WHERE evd_no = ' + str(evd_id) + ';'
+		self.src_path = db.execute_query(conn, query)
+
+		# Get Case & Evidence Name
+		query = 'SELECT case_name FROM tn_case WHERE case_no = ' + str(case_id) + ';'
+		case_name = db.execute_query(conn, query)
+
+		query = 'SELECT evd_name FROM tn_evidence WHERE evd_no = ' + str(evd_id) + ';'
+		evd_name = db.execute_query(conn, query)
+		db.close(conn)
+
+		# Create directory to store splitted image
+		self.dst_path = '/data/share/image' + '/' + case_name + '/' + evd_name + '/split'
+
+		if not os.path.exists(self.dst_path):
+			os.mkdir(self.dst_path)
+		
+		# Get partition list in image file
 		mediator = split_disk.DiskSpliterMediator()
 		disk_spliter = split_disk.DiskSpliter(mediator=mediator)
+		base_path_specs = disk_spliter.GetBasePathSpecs(self.src_path)
 
-		base_path_specs = disk_spliter.GetBasePathSpecs(source)
+	def Image_Analysis(self, case_id, evd_id, user_id):
+		print('Image Analyis')
 
 	def FileSystem_Analysis(self, case_id, evd_id):
 		print('FileSystem Analyis')
@@ -32,8 +57,7 @@ class CARPE_AM:
 def main():
 	try:
 		carpe_manager = CARPE_AM()
-		carpe_manager.Image_Analysis(1,2,3)
-		print('Start')
+		carpe_manager.Preprocess(1,1,1)
 	except KeyboardInterrupt:
 		print('Stop')
 
