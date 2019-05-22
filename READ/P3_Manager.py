8fb512c669507e2133cc84fdfcae9731242d51a4
kukheon <kukheon1109@gmail.com>
2019-05-22 14:13:23 +0900

    Add files via upload
    MultiProcess

--- READ/P3_Manager.py
+++ READ/P3_Manager.py
@@ -1,51 +1,49 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 """
 @author:    Kukheon Lee
 @contact:   kukheon1109@gmail.com
 """
+
 import os, sys
 import json
 from os import listdir
 from os.path import isfile, join
+from multiprocessing import Pool 
 sys.path.insert(0, "/READ/PDF")
 
 from PDF.carpe_pdf import PDF
 from OOXML.Carpe_OOXML import OOXML
 from MS_Office.carpe_compound import Compound
 
-class IITP3:
-    #각 모듈로 던져주기(+멀티프로세싱 처리 필요) 
-    def ModuleSelect(self, fileX):
-        nameChk, ext = os.path.splitext(fileX)  
-        if ext in '.pdf': #이선호 연구원 
-            with PDF(fileX) as pdf:
-                pdf.parse_content()
-                jsonS = json.dumps(pdf.content)
-                print(jsonS)
-        elif ext in ('.doc', '.xls', '.ppt'): 
-            object = Compound(fileX) 
-            object.parse()
-        elif ext in ('.docx', '.xlsx', '.pptx'): 
-            ooxml = OOXML(fileX)
-            ooxml.parse_ooxml()
-        elif ext in '.hwp': 
-            print('hwp')
-        else:
-            print('NONE')
+filePath = "C:\\Users\\kukheon\\Desktop\\test"
 
-def main():
-    try:
-        inPath = "C:\\Users\\kukheon\\Desktop\\test"
-        inFileList = [f for f in listdir(inPath) if isfile(join(inPath, f))]
-        iitp3_manager = IITP3()
+def push_analysis(file):
+    #print(file + ' ★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆ 분석시작')
+    target = os.path.join(filePath, file)
+    nameChk, extChk = os.path.splitext(target)
+
+    print(file + ' ★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆ ' + file)
+
+    if extChk in '.pdf': #pdf 파일처리
+        with PDF(target) as pdf:
+            pdf.parse_content()
+    elif extChk in ('.doc', '.xls', '.ppt'): #COMPOUND
+        object = Compound(target) 
+        object.parse()
+    elif extChk in ('.docx', '.xlsx', '.pptx'): 
+        object = OOXML(target)
+        object.parse_ooxml()
+    elif extChk in '.hwp': 
+        print('hwp')
     
-        for extChk in inFileList:
-            iitp3_manager.ModuleSelect(os.path.join(inPath, extChk))
+    #with open("C:\\Users\\kukheon\\Desktop\\output\\" + file+'.json', 'w', encoding='utf-8') as jsonOutput:
+        #jsonOutput.write(pdf.content)
 
-    except KeyboardInterrupt:
-        print('error')
+def get_filelist():
+    rtFileList =  [f for f in listdir(filePath) if isfile(join(filePath, f))]
+    return rtFileList
 
 if __name__ == "__main__":
-    main()
+
+    pool = Pool(processes=4)
+    pool.map(push_analysis, get_filelist())
+    print('END')
