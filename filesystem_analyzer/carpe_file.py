bbcc47ba2506e66f8e32797a496830003d810e82
crmsnmercury <crmsnmercury@korea.ac.kr>
2019-08-30 16:34:39 +0900

    Add files via upload

--- filesystem_analyzer/carpe_file.py
+++ filesystem_analyzer/carpe_file.py
@@ -6,6 +6,8 @@ class Carpe_File(object):
 	def __init__(self):
 		super(Carpe_File, self).__init__()
 		self._p_id = ""
+		#self._attr_type = 0
+		#self._attr_id = 0
 		self._inode = ""
 		self._name = ""
 		self._file_id = 0
@@ -15,32 +17,29 @@ class Carpe_File(object):
 		self._meta_type = 0
 		self._meta_flags = 0
 		self._size = 0
-		self._si_mtime = 0
-		self._si_atime = 0
-		self._si_ctime = 0
-		self._si_etime = 0
-		self._si_mtime_nano = 0
-		self._si_atime_nano = 0
-		self._si_ctime_nano = 0
-		self._si_etime_nano = 0
-		self._fn_mtime = 0
-		self._fn_atime = 0
-		self._fn_ctime = 0
-		self._fn_etime = 0		
-		self._fn_mtime_nano = 0
-		self._fn_atime_nano = 0
-		self._fn_ctime_nano = 0
-		self._fn_etime_nano = 0
+		self._mtime = 0
+		self._atime = 0
+		self._ctime = 0
+		self._etime = 0
+		self._mtime_nano = 0
+		self._atime_nano = 0
+		self._ctime_nano = 0
+		self._etime_nano = 0
+		self._additional_mtime = 0
+		self._additional_atime = 0
+		self._additional_ctime = 0
+		self._additional_etime = 0		
+		self._additional_mtime_nano = 0
+		self._additional_atime_nano = 0
+		self._additional_ctime_nano = 0
+		self._additional_etime_nano = 0
 		self._mode = 0
 		self._uid = 0
 		self._gid = 0
-		self._md5 = ""
-		self._sha1 = ""
-		self._sha256 = ""
+		self._hash = ""
 		self._parent_path = ""
 		self._parent_id = 0
 		self._extension = ""
-		self._bookmark = False
 
 	def toTuple(self):
 		var_list = sorted([x for x in dir(self) if (re.match(r'(^[_][a-z])', x))])
