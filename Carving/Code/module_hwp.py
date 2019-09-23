1403b397aa8e3d6b8160005695ee7453a74a0a2a
Gibartes <konm2000@hotmail.com>
2019-09-23 11:58:42 +0900

    Update carving modules

--- Carving/Code/module_hwp.py
+++ Carving/Code/module_hwp.py
@@ -1,8 +1,8 @@
 #-*- coding: utf-8 -*-
 import os, sys, struct
 
-from defines import ModuleConstant
-from interface       import ModuleComponentInterface
+from moduleInterface.defines   import ModuleConstant
+from moduleInterface.interface import ModuleComponentInterface
 
 
 class ModuleHWP(ModuleComponentInterface):
@@ -44,7 +44,8 @@ class ModuleHWP(ModuleComponentInterface):
 
     def carve(self):
         self.fp = open(self.get_attrib(ModuleConstant.FILE_ATTRIBUTE), 'rb')
-
+        self.fp.seek(self.get_attrib(ModuleConstant.IMAGE_BASE),os.SEEK_SET)
+        
         pvBuffer = b''
         strTmpFn = b''
         nRead = b''
--- Carving/Code/module_zip.py
+++ Carving/Code/module_zip.py
@@ -1,8 +1,8 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import os, sys, struct
 
-from defines import ModuleConstant
-from interface       import ModuleComponentInterface
+from moduleInterface.defines import ModuleConstant
+from moduleInterface.interface import ModuleComponentInterface
 
 
 class ModuleZIP(ModuleComponentInterface):
@@ -11,61 +11,64 @@ class ModuleZIP(ModuleComponentInterface):
     CONST_GIGABYTE = 1073741824
 
     CONST_RAMSLACK_COMP_UNIT = 2
-    CONST_COMPOUND_MAX = 50*CONST_MEGABYTE
-    CONST_SEARCH_SIZE = 200*CONST_MEGABYTE
-    CONST_BIG_SEARCH_SIZE = 200*CONST_MEGABYTE
+    CONST_COMPOUND_MAX = 50 * CONST_MEGABYTE
+    CONST_SEARCH_SIZE = 200 * CONST_MEGABYTE
+    CONST_BIG_SEARCH_SIZE = 200 * CONST_MEGABYTE
 
     uClusterSize = 0x1000
 
     def __init__(self):
-        super().__init__()                  # Initialize Module Interface
-        self.fileSize   = 0
-        self.offset     = list()
+        super().__init__()  # Initialize Module Interface
+        self.fileSize = 0
+        self.offset = list()
 
-        self.set_attrib(ModuleConstant.NAME,"ZIP")
-        self.set_attrib(ModuleConstant.VERSION,"0.1")
-        self.set_attrib(ModuleConstant.AUTHOR,"JH")
+        self.set_attrib(ModuleConstant.NAME, "ZIP")
+        self.set_attrib(ModuleConstant.VERSION, "0.1")
+        self.set_attrib(ModuleConstant.AUTHOR, "JH")
 
         self.fp = None
-
 
     """ Module Methods """
 
     def __evaluate(self):
         fn = self.attrib.get(ModuleConstant.FILE_ATTRIBUTE)
-        if(fn==None):
+        if (fn == None):
             return ModuleConstant.Return.EINVAL_ATTRIBUTE
         try:
-            fd = os.open(fn,os.O_RDONLY)
+            fd = os.open(fn, os.O_RDONLY)
             os.close(fd)
-        except:return ModuleConstant.Return.EINVAL_FILE
+        except:
+            return ModuleConstant.Return.EINVAL_FILE
         return ModuleConstant.Return.SUCCESS
 
     def carve(self):
         self.fp = open(self.get_attrib(ModuleConstant.FILE_ATTRIBUTE), 'rb')
+        self.fp.seek(self.get_attrib(ModuleConstant.IMAGE_BASE), os.SEEK_SET)
         i = 0
         totalfilesize = 0
 
-        while( i <= self.get_attrib(ModuleConstant.IMAGE_LAST) - self.get_attrib(ModuleConstant.IMAGE_BASE)) :   # 100MB
-            self.fp.seek(i)
+        while (i <= self.get_attrib(ModuleConstant.IMAGE_LAST) - self.get_attrib(ModuleConstant.IMAGE_BASE)):  # 100MB
+            self.fp.seek(self.get_attrib(ModuleConstant.IMAGE_BASE) + i, os.SEEK_SET)
             temp = self.fp.read(0x40)
-            self.fp.seek(i)
-            if(temp[0:2] != b'\x50\x4B'):
+            self.fp.seek(self.get_attrib(ModuleConstant.IMAGE_BASE) + i, os.SEEK_SET)
+
+            if (temp[0:2] != b'PK'):
+                # if(temp[0:2] != b'\x50\x4B'):
                 self.offset = (False, 0, -1, ModuleConstant.INVALID)
                 return False
             # 사이즈 나눠야함, 시그니처 별로.
             # 파일 사이즈 다음에 0x04034b50이 있으면 압축된 파일 더 있음
-            if(temp[0:2] == b'\x50\x4B' and temp[2:4] == b'\x03\x04'):
+            if (temp[0:2] == b'\x50\x4B' and temp[2:4] == b'\x03\x04'):
                 temp = self.fp.read(0x1E)
                 zipheader_comsize = struct.unpack('<I', temp[0x12:0x16])[0]
                 zipheader_namelength = struct.unpack('<H', temp[0x1A:0x1C])[0]
                 zipheader_extralength = struct.unpack('<H', temp[0x1C:0x1E])[0]
 
-                filesize = 0x1E + zipheader_namelength + zipheader_extralength + zipheader_comsize    # 0x1D is header size
+                filesize = 0x1E + zipheader_namelength + zipheader_extralength + zipheader_comsize  # 0x1D is header size
                 totalfilesize += filesize
                 i = totalfilesize
                 continue
-            #중심 디렉토리인 경우
+            # 중심 디렉토리인 경우
             elif (temp[0:2] == b'\x50\x4B' and temp[2:4] == b'\x01\x02'):
                 temp = self.fp.read(0x2E)
                 zipcentral_namelength = struct.unpack('<H', temp[0x1C:0x1E])[0]
@@ -76,7 +79,7 @@ class ModuleZIP(ModuleComponentInterface):
                 totalfilesize += filesize
                 i = totalfilesize
                 continue
-            #Digital signature인 경우
+            # Digital signature인 경우
             elif (temp[0:2] == b'\x50\x4B' and temp[2:4] == b'\x05\x05'):
                 temp = self.fp.read(0x06)
                 zipdigitalsignature_datasize = struct.unpack('<H', temp[0x04:0x06])[0]
@@ -85,7 +88,7 @@ class ModuleZIP(ModuleComponentInterface):
                 totalfilesize += filesize
                 i = totalfilesize
                 continue
-            #Zip64 end of Central directory record인 경우0x06064b50
+            # Zip64 end of Central directory record인 경우0x06064b50
             elif (temp[0:2] == b'\x50\x4B' and temp[2:4] == b'\x06\x06'):
                 temp = self.fp.read(0x38)
                 zip64cdr_sizezip64 = struct.unpack('<Q', temp[0x04:0x0C])[0]
@@ -94,9 +97,9 @@ class ModuleZIP(ModuleComponentInterface):
                 totalfilesize += filesize
                 i = totalfilesize
                 continue
-            #Zip64 end of Central directory locator인 경우0x07064b50
+            # Zip64 end of Central directory locator인 경우0x07064b50
             elif (temp[0:2] == b'\x50\x4B' and temp[2:4] == b'\x06\x07'):
-                filesize = 0x14                 # 0x14 is header size
+                filesize = 0x14  # 0x14 is header size
                 totalfilesize += filesize
                 i = totalfilesize
                 continue
@@ -105,55 +108,51 @@ class ModuleZIP(ModuleComponentInterface):
                 filesize = 0x16
                 totalfilesize += filesize
                 i = totalfilesize
-                print(totalfilesize, hex(totalfilesize))
-                self.offset = (True, self.attrib.get(ModuleConstant.IMAGE_BASE), totalfilesize, ModuleConstant.FILE_ONESHOT)        # Success
+                self.offset = (
+                True, self.attrib.get(ModuleConstant.IMAGE_BASE), totalfilesize, ModuleConstant.FILE_ONESHOT)  # Success
                 return True
             else:
-                self.offset = (True, self.attrib.get(ModuleConstant.IMAGE_BASE), totalfilesize, ModuleConstant.FILE_ONESHOT)        # Success
-                print(totalfilesize, hex(totalfilesize))
+                self.offset = (
+                True, self.attrib.get(ModuleConstant.IMAGE_BASE), totalfilesize, ModuleConstant.FILE_ONESHOT)  # Success
                 return True
 
-            self.offset = (False, 0, -1, ModuleConstant.INVALID)        # Fail
+            self.offset = (False, 0, -1, ModuleConstant.INVALID)  # Fail
             return False
-
-
-
-
 
     """ Interfaces """
 
-    def module_open(self,id):               # Reserved method for multiprocessing
+    def module_open(self, id):  # Reserved method for multiprocessing
         super().module_open()
 
-    def module_close(self):                 # Reserved method for multiprocessing
+    def module_close(self):  # Reserved method for multiprocessing
         pass
 
-    def set_attrib(self,key,value):         # 모듈 호출자가 모듈 속성 변경/추가하는 method interface
-        self.update_attrib(key,value)
+    def set_attrib(self, key, value):  # 모듈 호출자가 모듈 속성 변경/추가하는 method interface
+        self.update_attrib(key, value)
 
-    def get_attrib(self,key,value=None):    # 모듈 호출자가 모듈 속성 획득하는 method interface
+    def get_attrib(self, key, value=None):  # 모듈 호출자가 모듈 속성 획득하는 method interface
         return self.attrib.get(key)
 
-    def execute(self,cmd=None,option=None): # 모듈 호출자가 모듈을 실행하는 method
+    def execute(self, cmd=None, option=None):  # 모듈 호출자가 모듈을 실행하는 method
         ret = self.__evaluate()
-        if(ret!=ModuleConstant.Return.SUCCESS):
-            return [(False,ret,ModuleConstant.INVALID)]
+        if (ret != ModuleConstant.Return.SUCCESS):
+            return [(False, ret, ModuleConstant.INVALID)]
         self.carve()
-        if(self.offset==[]):
-            return [(False,0,ModuleConstant.INVALID)]
-        return self.offset                  # return <= 0 means error while collecting information
+        if (self.offset == []):
+            return [(False, 0, ModuleConstant.INVALID)]
+        return self.offset  # return <= 0 means error while collecting information
 
 
 if __name__ == '__main__':
 
     file = ModuleZIP()
     try:
-        file.set_attrib(ModuleConstant.FILE_ATTRIBUTE,sys.argv[1])   # Insert File
+        file.set_attrib(ModuleConstant.FILE_ATTRIBUTE, sys.argv[1])  # Insert File
     except:
         print("This moudule needs exactly one parameter.")
         sys.exit(1)
 
-    file.set_attrib(ModuleConstant.IMAGE_BASE,0)  # Set offset of the file base
+    file.set_attrib(ModuleConstant.IMAGE_BASE, 0)  # Set offset of the file base
     file.set_attrib(ModuleConstant.IMAGE_LAST, ModuleZIP.CONST_SEARCH_SIZE)  # Set offset of the file last
     cret = file.execute()
     print(cret)
