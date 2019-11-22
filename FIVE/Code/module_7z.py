b80744b96bbfd90ddcaa54df60ece98e1331ac10
Gibartes <konm2000@hotmail.com>
2019-11-22 16:46:06 +0900

    Update 7z carving module

--- FIVE/Code/module_7z.py
+++ FIVE/Code/module_7z.py
@@ -71,7 +71,6 @@ class Module7Z(ModuleComponentInterface):
         if(self.parser.get_value('signature')!=Structure7Z.SIGNATURE):
             self.parser.cleanup()
             return
-        self.parser.print()
 
         next_hdr_off  = self.parser.byte2int(self.parser.get_value("next_hdr_off"))
         next_hdr_size = self.parser.byte2int(self.parser.get_value("next_hdr_size"))
