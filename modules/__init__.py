d6a1ccfa433012340aa40e9db38e14f49e56bd64
MAX\max <max.joun@gmail.com>
2020-12-17 21:36:33 +0900

    APFS_fixed

--- modules/__init__.py
+++ modules/__init__.py
@@ -31,6 +31,7 @@ from modules import google_drive_connector
 from modules import google_drive_entry_connector
 from modules import google_drive_fs_connector
 from modules import google_drive_volume_connector
+from modules import macos_connector
 
 from modules import fica
 from modules import extract
