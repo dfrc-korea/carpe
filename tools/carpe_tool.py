0e591336c73a5d4ec1d7dbbd268caa7decdee02f
KimVegetable <rlawnsgh92@korea.ac.kr>
2021-02-08 17:56:22 +0900

    update carpe

--- tools/carpe_tool.py
+++ tools/carpe_tool.py
@@ -311,17 +311,12 @@ class CarpeTool(extraction_tool.ExtractionTool,
 
             # parse advanced modules
             engine.ProcessAdvancedModules(configuration)
-
-            # carve
-            print("Carving Start")
-            if not self._partition_list:
-                print("No partition")
-                engine.process_carve(configuration, is_partition=False)
                 
             if configuration.source_path_specs[0].TYPE_INDECATOR == 'APFS':
                 pass
             else:
                 # carve
+                print("Carving Start")
                 if not self._partition_list:
                     print("No partition")
                     engine.process_carve(configuration, is_partition=False)
