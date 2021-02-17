2d51f3482356f4c5c3476887552aca08dd36a59e
MAX\max <max.joun@gmail.com>
2021-02-17 15:35:35 +0900

    evt_total fixed

--- modules/Eventlog/lv1_os_win_evt_total.py
+++ modules/Eventlog/lv1_os_win_evt_total.py
@@ -28,19 +28,19 @@ def EventlogTotal(real_file_path, filename):
             except:
                 continue
 
-            root = XML.fromstring(xml_str)  # Event Tag
-            assert len(XML._namespaces(root)) == 2
+            if xml_str != '':
+                root = XML.fromstring(xml_str)  # Event Tag
+                assert len(XML._namespaces(root)) == 2
 
-            event_total_information = Eventlog_Total_Information()
-            result.append(event_total_information)
-            result[eventlog_count].event_id = root[0][1].text
-            if 'TimeCreated' in root[0][5].tag:
-                result[eventlog_count].time_created = root[0][5].get('SystemTime').replace(' ', 'T') + 'Z'
-            else:
-                result[eventlog_count].time_created = root[0][7].get('SystemTime').replace(' ', 'T') + 'Z'
-            result[eventlog_count].user_sid = root[0][-1].get('UserID')
-            result[eventlog_count].source = real_file_path
-            result[eventlog_count].data = html.unescape(xml_str)
-            eventlog_count = eventlog_count + 1
-
+                event_total_information = Eventlog_Total_Information()
+                result.append(event_total_information)
+                result[eventlog_count].event_id = root[0][1].text
+                if 'TimeCreated' in root[0][5].tag:
+                    result[eventlog_count].time_created = root[0][5].get('SystemTime').replace(' ', 'T') + 'Z'
+                else:
+                    result[eventlog_count].time_created = root[0][7].get('SystemTime').replace(' ', 'T') + 'Z'
+                result[eventlog_count].user_sid = root[0][-1].get('UserID')
+                result[eventlog_count].source = real_file_path
+                result[eventlog_count].data = html.unescape(xml_str)
+                eventlog_count = eventlog_count + 1
     return result
